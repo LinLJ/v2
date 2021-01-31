3b2ddd70a5e087c096b99889ad3037e61cb1edf8
qiuzhanghua <qiuzhanghua@icloud.com>
2021-01-31 20:28:56 +0800

    vuejs sass

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <p>{{ this.sayHello() }}</p>
+    <a :href="link">qiuzhanghua</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return { title: "Hello Vue.js 2!", link: "https://github.com/qiuzhanghua" };
+  },
+  methods: {
+    sayHello() {
+      return this.title;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+$bg_color: blue;
+
+body {
+  background-color: purple;
+}
+
+a {
+  color: white;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+  background-color: $bg_color;
+}
+</style>
