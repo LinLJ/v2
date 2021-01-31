3f10b4a1115292cd0263e7a9921d10afdc1f9d07
qiuzhanghua <qiuzhanghua@icloud.com>
2021-01-31 21:08:07 +0800

    event

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="app">
+  <div id="app" @mousemove="updatePos">
     <p v-once>{{ this.sayHello() }}</p>
     <a :href="link">qiuzhanghua</a>
     <p v-html="boldText"></p>
+    <button @click="inc(5)">Click me</button>
+    <p>{{ count }}</p>
+    <p>x = {{ x }}, y = {{ y }}</p>
   </div>
 </template>
 
@@ -14,11 +17,22 @@ export default {
       title: "Hello Vue.js 2!",
       link: "https://github.com/qiuzhanghua",
       boldText: "<b>Bold Text</b>",
+      count: 0,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     sayHello() {
       return this.title;
+    },
+    inc(d = 1) {
+      this.count += d;
+    },
+    updatePos(event) {
+      // console.log(event);
+      this.x = event.clientX;
+      this.y = event.clientY;
     },
   },
 };
@@ -28,7 +42,7 @@ export default {
 $bg_color: blue;
 
 body {
-  background-color: purple;
+  background-color: lightblue;
 }
 
 a {
