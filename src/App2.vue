b5065bb3a721e2909404ebe19ab560e5ac365aab
qiuzhanghua <qiuzhanghua@icloud.com>
2021-02-01 10:29:38 +0800

    two vue instance

---
+++ src/App2.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>{{ title }}</h3>
+</template>
+
+<script>
+export default {
+  name: "App2",
+  data() {
+    return { title: "The Second Vue Instance" };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import App2 from "./App2.vue";
 
 Vue.config.productionTip = false;
 
-new Vue({
+let vm1 = new Vue({
   el: "#app",
   render: (h) => h(App),
 });
+
+new Vue({
+  el: "#app2",
+  render: (createElement) => createElement(App2),
+});
+
+console.log(vm1);
